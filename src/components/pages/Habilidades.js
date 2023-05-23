1c1c0901fed7d0e4500957ea9cef930c32284ded
Luan Fernandes <izetaz50@gmail.com>
2023-05-23 10:51:22 -0300

    att

--- src/components/pages/Habilidades.js
+++ src/components/pages/Habilidades.js
@@ -2,6 +2,7 @@ import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { DiJavascript1 } from "react-icons/di";
 import { FaReact } from "react-icons/fa";
+import { BsGit } from "react-icons/bs"
 
 import styles from './Habilidades.module.css'
 import { useState } from "react";
@@ -11,7 +12,7 @@ const Habilidades = ({ modoDL }) => {
   const [tituloTec, setTituloTec] = useState(undefined);
   const [paragrafoTec, setParagrafoTec] = useState(undefined);
   if(tituloTec === undefined){
-    setTituloTec("Passe o mouse por cima dos icones...")
+    setTituloTec("Passe o mouse ou click em algum dos icones...")
   }
 
   return (
@@ -25,10 +26,12 @@ const Habilidades = ({ modoDL }) => {
         <DiJavascript1 onMouseEnter={() => [setTituloTec("JavaScript"),setParagrafoTec("texto do JS")]} onMouseLeave={() => [setTituloTec(undefined),setParagrafoTec(undefined)]} className={modoDL === true ? styles.iconjsD : styles.iconjs}/>
 
       <FaReact onMouseEnter={() => [setTituloTec("React"),setParagrafoTec("texto do react")]} onMouseLeave={() => [setTituloTec(undefined),setParagrafoTec(undefined)]} className={modoDL === true ? styles.iconreactD : styles.iconreact}/>
+
+      <BsGit onMouseEnter={() => [setTituloTec("Git"),setParagrafoTec("Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum molestias sunt, aliquid earum ad nostrum distinctio. Ab maxime facere quibusdam nobis cupiditate repudiandae aspernatur sunt enim, in optio recusandae natus!")]} onMouseLeave={() => [setTituloTec(undefined),setParagrafoTec(undefined)]} className={modoDL === true ? styles.iconGitD : styles.iconGit}/>
       </div>
         <span className={modoDL === true ? styles.textotecD : styles.textotec}>
             <h1>{tituloTec}</h1>
-            <p>{paragrafoTec}</p>
+            <p>{paragrafoTec}  </p>
         </span>
     </div>
   )
